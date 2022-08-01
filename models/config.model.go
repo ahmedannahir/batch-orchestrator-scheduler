6594b3c4131f7e56fc70c5b2d1c70b1f29ffffea
Ahmed Annahir <a.annahir@gmail.com>
2022-08-01 16:33:12 +0000

    added batch independancy option

--- models/config.model.go
+++ models/config.model.go
@@ -4,5 +4,6 @@ type Config struct {
 	Cron         string   `json:"cron"`
 	Language     string   `json:"language"`
 	Script       string   `json:"script"`
+	Independant  bool     `json:"independant"`
 	Dependencies []string `json:"dependencies"`
 }
